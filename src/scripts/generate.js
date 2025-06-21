5451ecb6a59ad17b85b713d17ca8f740a6db2ec0
= <you@example.com>
2025-06-22 01:08:01 +0530

    removed skip btn

--- src/scripts/generate.js
+++ src/scripts/generate.js
@@ -1,28 +1,11 @@
 function generateLink() {
     const urlParams = new URLSearchParams(window.location.search);
-    const link = urlParams.get('link');
-    let timeoutId;
-
-    function navigate() {
-        let url = link;
-        if (!/^https?:\/\//i.test(link)) {
-            url = `https://${link}`;
-        }
-        window.location.href = url;
-    }
 
     if (link) {
         // Delay for 6 seconds before navigating to the link
-        timeoutId = setTimeout(navigate, 6000);
-
-        // Add event listener for the Skip button
-        const skipBtn = document.getElementById('skipBtn');
-        if (skipBtn) {
-            skipBtn.addEventListener('click', function() {
-                clearTimeout(timeoutId);
-                navigate();
-            });
-        }
+        setTimeout(() => {
+            window.location.href = `https://${link}`;
+        }, 6000);
     } else {
         document.body.innerHTML = '<h1>No link provided!</h1>';
     }
