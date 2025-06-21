43ef37e9dc189eab7a743f5a3d6dbbb8c13ad719
= <you@example.com>
2025-06-22 00:47:36 +0530

    link fix

--- src/scripts/generate.js
+++ src/scripts/generate.js
@@ -1,17 +1,11 @@
 function generateLink() {
     const urlParams = new URLSearchParams(window.location.search);
-    const link = urlParams.get('link');
-    alert(`Link: ${link}`);
 
     if (link) {
         // Delay for 4 seconds before navigating to the link
         setTimeout(() => {
-            let url = link;
-            if (!/^https?:\/\//i.test(link)) {
-                url = `https://${link}`;
-            }
-            window.location.href = url;
-        }, 5000);
+            window.location.href = `https://${link}`;
+        }, 4000);
     } else {
         document.body.innerHTML = '<h1>No link provided!</h1>';
     }
